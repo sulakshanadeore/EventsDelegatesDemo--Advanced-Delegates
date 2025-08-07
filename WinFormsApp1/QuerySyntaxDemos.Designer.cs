c616ca5dcba1d4ec9d14eec84f0c31d327e35e22
sai <sai_prasad_veluri@yahoo.com>
2025-08-07 05:05:54 +0000

    Query Syntax

---
+++ WinFormsApp1/QuerySyntaxDemos.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WinFormsApp1
+{
+    partial class QuerySyntaxDemos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(42, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Working witht Array";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(42, 87);
+            listBox1.Name = "listBox1";
+            listBox1.ScrollAlwaysVisible = true;
+            listBox1.Size = new Size(421, 224);
+            listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(287, 38);
+            button2.Name = "button2";
+            button2.Size = new Size(176, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Working With List";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // QuerySyntaxDemos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Name = "QuerySyntaxDemos";
+            Text = "QuerySyntaxDemos";
+            Load += QuerySyntaxDemos_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp1/QuerySyntaxDemos.cs
@@ -0,0 +1,94 @@
+﻿using RetailLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class QuerySyntaxDemos : Form
+    {
+        public QuerySyntaxDemos()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = new int[] { 1, 23, 42, 563, 2566, 255, 345, 78, 90, 133, 334 };
+            var evenNos = from n in numbers
+                          orderby n descending
+                          where n % 2 == 0
+                          select n;
+
+            numbers[0] = 100;
+            numbers[1] = 120;
+
+            foreach (var item in evenNos)
+            {
+                listBox1.Items.Add(item);
+            }
+
+        }
+        List<Employee> employees = new List<Employee>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            employees.Add(new Employee { Empid = 10, EmpName = "Sara", City = "Pune", Salary = 20000, Deptno = 10 });
+            employees.Add(new Employee { Empid = 11, EmpName = "Piya", City = "Chennai", Salary = 30000, Deptno = 20 });
+            employees.Add(new Employee { Empid = 12, EmpName = "Kia", City = "Bangalore", Salary = 5000, Deptno = 30 });
+            employees.Add(new Employee { Empid = 13, EmpName = "Hari", City = "Pune", Deptno = 10 });
+            employees.Add(new Employee { Empid = 14, EmpName = "Simran", City = "Pune", Deptno = 10 });
+            employees.Add(new Employee { Empid = 15, EmpName = "Raj", City = "Pune", Deptno = 20 });
+
+
+            //var grouped = from e1 in employees
+            //              group e1 by e1.Deptno into g
+            //              select new 
+            //              {
+            //                  Department=g.Key, 
+            //                  EmpCount=g.Count(), 
+            //                  emplist=from edata in g
+            //                             select new {EmployeeID= edata.Empid, EmployeeName=edata.EmpName }
+            //              };
+
+           // select g;
+
+
+            var grouped = employees.GroupBy(e1 => e1.Deptno).
+                Select(e1 => new
+                {
+                    Department = e1.Key,
+                    EmpCount = e1.Count(),
+                    emplist = e1.Select(y1 => new {EmployeeID=y1.Empid, EmployeeName= y1.EmpName
+                   ,EmpSalary =y1.Salary
+                    })
+                }); 
+
+            employees.Add(new Employee { Empid = 16, EmpName = "Vishal", City = "Pune", Deptno = 10 });
+            employees.Add(new Employee { Empid = 17, EmpName = "Rima", City = "Pune", Deptno = 20 });
+
+
+            foreach (var item in grouped)
+            {
+                listBox1.Items.Add("Deptno=" + item.Department + " and Employees= " + item.EmpCount );
+                //    listBox1.Items.Add("Deptno=" + item.Key + " and Employees= " + item.Count());
+                foreach (var item1 in item.emplist)
+                {
+                   // listBox1.Items.Add(item1.Empid + " " + item1.EmpName + item1.Salary);
+                    listBox1.Items.Add(item1.EmployeeID + " " + item1.EmployeeName + " " + item1.EmpSalary);
+                }
+
+                
+
+            }
+
+
+        }
+    }
+}
